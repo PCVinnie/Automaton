9a0d8db0f0aaf907ba29b1dbc7de692e71bf64c6
PCVinnie <vastout@vasnet.nl>
2015-06-10 13:29:34 +0200

    Add states and symbols to Program.cs.

--- Automaton/Program.cs
+++ Automaton/Program.cs
@@ -11,6 +11,21 @@ namespace Automaton
         static void Main(string[] args)
         {
 
+            State stateA = new State('A', 0);
+            State stateB = new State('B', 1);
+            State stateC = new State('C', 2);
+
+            List<State> myStates = new List<State>();
+            myStates.Add(stateA);
+            myStates.Add(stateB);
+            myStates.Add(stateC);
+
+            List<char> mySymbols = new List<char>();
+            mySymbols.Add('a');
+            mySymbols.Add('b');
+
+            Automaton automaton = new Automaton(myStates,mySymbols);
+
             Console.WriteLine("Formele methode");
             Console.WriteLine("Vincent Stout & Lesley van Hoek");
             Console.WriteLine("-------------------------------");
